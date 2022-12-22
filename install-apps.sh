794240f2c6b8dfe91fa65b4a7cbcb3ac557ca955
1lukass <lukaswatkin@gmail.com>
2022-12-22 15:24:55 -0800

    added '-y' option

--- install-apps.sh
+++ install-apps.sh
@@ -4,10 +4,10 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install arduino audacity code minicom nmap vlc wireshark
+sudo apt install arduino audacity code minicom nmap python3.8 vlc wireshark -y
 
 #install brave browser
-sudo apt install curl
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
